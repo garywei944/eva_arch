2003dfc3355b8db020fe124c7e635cb6bb2c78fa
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-14 01:42:38 -0400

    Update autostart.sh

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# [[ $(pgrep compton) ]] || compton -b --backend glx --vsync opengl-swc
-[[ $(pgrep picom) ]] || picom -b
-[[ $(pgrep chrome) ]] || google-chrome-stable --password-store=gnome --no-startup-window &
+([[ $(pgrep picom) ]] || picom -b) &
+([[ $(pgrep chrome) ]] || google-chrome-stable --password-store=gnome --no-startup-window) &
 # [[ $(pgrep albert) ]] || (unset XDG_CURRENT_DESKTOP; albert) &
 # [[ $(pgrep albert) ]] || albert &
-[[ $(pgrep krunner) ]] || krunner -d
+([[ $(pgrep fcitx) ]] || fcitx -d) &
+([[ $(pgrep krunner) ]] || krunner -d) &
 numlockx on
 waw
