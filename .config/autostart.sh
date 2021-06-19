97559fcfdfc6a45cb0ad56c9c4d50195f401f413
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-19 01:50:25 -0400

    Start albert immediately

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -8,11 +8,11 @@ numlockx on
 waw
 __spawn picom picom -b
 __spawn fcitx fcitx -d
+__spawn albert albert
 
 sleep 1
 
 __spawn chrome google-chrome-stable --password-store=gnome --no-startup-window
 __spawn insync insync start
-__spawn albert albert
 __spawn deja-dup deja-dup --gapplication-service
 # __spawn krunner krunner -d
