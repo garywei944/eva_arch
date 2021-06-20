9f7c07feec108e5416480aa935f95ead9a3e1892
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-20 01:31:33 -0400

    Change .bin to bin

--- .config/zsh_custom/plugins/conda/conda.plugin.zsh
+++ .config/zsh_custom/plugins/conda/conda.plugin.zsh
@@ -28,13 +28,13 @@ alias cuc="conda update conda"
 alias cua="conda update --all"
 alias cuf="conda env update -f"
 
-function clink() {
+clink() {
 	local link
 	link=$(wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p')
 	echo $link
 }
 
-function cver() {
+cver() {
 	local version
 	version=$(echo $(clink) | sed -ne 's/.*Anaconda3-\(.*\)-Linux-x86_64.sh/\1/p')
 	echo $version
