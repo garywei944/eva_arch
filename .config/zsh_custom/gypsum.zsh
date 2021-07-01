afd8e6139e55a44f5062b9b5073ff0fe531e72fd
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-30 23:53:34 -0400

    Update gypsum and .envrc

--- .config/zsh_custom/gypsum.zsh
+++ .config/zsh_custom/gypsum.zsh
@@ -1,4 +1,10 @@
-alias gypsum='ssh guanghaowei@gypsum.cs.umass.edu'
+gypsum() {
+	if [[ $(hostname) == selenite.cs.umass.edu ]]; then
+		ssh guanghaowei@gypsum.cs.umass.edu
+	else
+		ssh guanghaowei@gypsum-gateway.cs.umass.edu
+	fi
+}
 
 alias cdw='/mnt/nfs/work1/hongyu/guanghaowei'
 alias noteaid='cd /mnt/nfs/work1/hongyu/guanghaowei/'
