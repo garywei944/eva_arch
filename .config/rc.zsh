301a1daf7da6946f552b13f403f92ba7236ed9ab
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-30 20:51:15 -0400

    Fix box_out doesn't work on zsh 5.0.2

--- .config/rc.zsh
+++ .config/rc.zsh
@@ -137,8 +137,16 @@ box_out() {
 
 if [[ -n $(command -v figlet) && -n $(command -v lolcat) ]]; then
 	echo "$(echo "ariseus" | figlet)\nWelcome back, ariseus." | lolcat
-elif [[ -n $(command -v lolcat) ]]; then
-	box_out "Welcome back, ariseus." | lolcat
+elif is-at-least 5.8; then
+	if [[ -n $(command -v lolcat) ]]; then
+		box_out "Welcome back, ariseus." | lolcat
+	else
+		box_out "Welcome back, ariseus."
+	fi
 else
-	box_out "Welcome back, ariseus."
+	echo " ------------------------
+|                        |
+| Welcome back, ariseus. |
+|                        |
+ ------------------------"
 fi
