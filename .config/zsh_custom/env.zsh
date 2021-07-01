12a58b0c3dfe88477e07cd374665a8e2bcdde245
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-07-01 18:10:45 -0400

    Fix server exit if no conda found

--- .config/zsh_custom/env.zsh
+++ .config/zsh_custom/env.zsh
@@ -1,14 +1,14 @@
-[[ -z ${CONDA_PATH+x} ]] && exit
-
 # Conda init
-__conda_setup="$('$CONDA_PATH/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [[ $? -eq 0 ]]; then
-	eval "$__conda_setup"
-else
-	if [[ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]]; then
-		source "$CONDA_PATH/etc/profile.d/conda.sh"
+if [[ -n ${CONDA_PATH+x} ]]; then
+	__conda_setup="$('$CONDA_PATH/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+	if [[ $? -eq 0 ]]; then
+		eval "$__conda_setup"
 	else
-		export PATH="$CONDA_PATH/bin:$PATH"
+		if [[ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]]; then
+			source "$CONDA_PATH/etc/profile.d/conda.sh"
+		else
+			export PATH="$CONDA_PATH/bin:$PATH"
+		fi
 	fi
+	unset __conda_setup
 fi
-unset __conda_setup
--- .config/zsh_custom/gypsum.zsh
+++ .config/zsh_custom/gypsum.zsh
@@ -1,20 +1,22 @@
-alias gps='ssh guanghaowei@gypsum.cs.umass.edu'
-alias gpsg='ssh guanghaowei@gypsum-gateway.cs.umass.edu'
+alias selenite='ssh guanghaowei@gypsum-gateway.cs.umass.edu'
+alias gypsum='ssh guanghaowei@gypsum.cs.umass.edu'
 
-alias gpcd='/mnt/nfs/work1/hongyu/guanghaowei'
+if [[ $(hostname) == node* ]]; then
+	alias gpcd='cd /mnt/nfs/work1/hongyu/guanghaowei'
 
-alias gpb='sbatch'
+	alias gpb='sbatch'
 
-alias gpsh='srun --pty /bin/zsh'
-alias gpshg='srun --pty --gres=gpu:1 /bin/zsh'
-alias gpshgl='srun --pty --gres=gpu:1 --partition=titanx-long /bin/zsh'
+	alias gpsh='srun --pty /bin/zsh'
+	alias gpshg='srun --pty --gres=gpu:1 /bin/zsh'
+	alias gpshgl='srun --pty --gres=gpu:1 --partition=titanx-long /bin/zsh'
 
-alias gpq='squeue | grep'
-alias gpps='squeue | grep guanghaowei'
+	alias gpq='squeue | grep'
+	alias gpps='squeue | grep guanghaowei'
 
-alias gpc='scancel'
+	alias gpc='scancel'
 
-alias gpi='sinfo'
+	alias gpi='sinfo'
+fi
 
 
 alias noteaid='cd ~/projects/NoteAid-Annotation; conda activate noteaid'
