3d662427af7ff478219ce5d24ed75108ef7b8c3f
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-07-01 01:38:28 -0400

    Update gypsum alias

--- .config/zsh_custom/gypsum.zsh
+++ .config/zsh_custom/gypsum.zsh
@@ -1,14 +1,20 @@
-alias gypsum='ssh guanghaowei@gypsum.cs.umass.edu'
-alias ggypsum='ssh guanghaowei@gypsum-gateway.cs.umass.edu'
+alias gps='ssh guanghaowei@gypsum.cs.umass.edu'
+alias gpsg='ssh guanghaowei@gypsum-gateway.cs.umass.edu'
 
-alias gpnoteaid='cd /mnt/nfs/work1/hongyu/guanghaowei/NoteAid-Annotation && conda activate noteaid'
 alias gpcd='/mnt/nfs/work1/hongyu/guanghaowei'
 
 alias gpb='sbatch'
+
 alias gpsh='srun --pty /bin/zsh'
 alias gpshg='srun --pty --gres=gpu:1 /bin/zsh'
 alias gpshgl='srun --pty --gres=gpu:1 --partition=titanx-long /bin/zsh'
+
 alias gpq='squeue | grep'
 alias gpps='squeue | grep guanghaowei'
+
 alias gpc='scancel'
+
 alias gpi='sinfo'
+
+
+alias gpnoteaid='cd /mnt/nfs/work1/hongyu/guanghaowei/NoteAid-Annotation && conda activate noteaid'
