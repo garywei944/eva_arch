41abb800f6a2d4b633c8128ab5ee110c85df640c
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-07-30 15:21:52 -0400

    Remove autostart evernote

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -20,4 +20,3 @@ __spawn chrome google-chrome-stable --password-store=gnome --no-startup-window
 __spawn insync insync start
 __spawn deja-dup deja-dup --gapplication-service
 __nokde_spawn birdtrap birdtray
-__nokde_spawn Evernote-Beta Evernote-Beta
