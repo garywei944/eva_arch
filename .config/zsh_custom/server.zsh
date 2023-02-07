44e873dc6e59ef1334aa6666eb65043906b7cd6d
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-02-07 02:24:03 -0500

    Update alias

--- .config/zsh_custom/server.zsh
+++ .config/zsh_custom/server.zsh
@@ -4,8 +4,11 @@
 alias sachiel='ssh root@sachiel.ariseus.net'
 alias shamshel='ssh ubuntu@shamshel.ariseus.net'
 
-alias ariseus='ssh aris@arisues.oasis.eva'
+alias nora='ssh aris@oasis.ariseus.net -p2234'
+alias ariseus='ssh aris@oasis.ariseus.net -p2211'
+
+# LAN, inside oasis
+alias ariseusl='ssh aris@arisues.oasis.eva'
 alias amiya='ssh guanghaowei@amiya.oasis.eva'
 alias noral='ssh aris@nora.oasis.eva'
-alias nora='ssh aris@oasis.ariseus.net -p2234'
 alias oasis='ssh -p2233 aris@nas.oasis.eva'
