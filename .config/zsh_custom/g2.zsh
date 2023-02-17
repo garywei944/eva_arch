56c3e11c5cd31336c50c4f5c66fd43c1be016237
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-02-17 00:58:10 -0500

    Add cuvpn

--- .config/zsh_custom/g2.zsh
+++ .config/zsh_custom/g2.zsh
@@ -3,3 +3,7 @@
 alias g2login='ssh gw338@g2-login.coecis.cornell.edu'
 alias gcpulow='srun -p default_partition-interactive --pty -n 4 --mem=8g /bin/zsh'
 alias gcpunode='srun -p desa_partition-interactive /bin/zsh'
+
+alias cuvpn='/opt/cisco/anyconnect/bin/vpn -s connect cuvpn.cuvpn.cornell.edu'
+alias cudisc='/opt/cisco/anyconnect/bin/vpn -s disconnect'
+alias custate='/opt/cisco/anyconnect/bin/vpn -s state'
