9917d957efd4d8fa6270880115992756bb637df2
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-03-21 05:24:40 -0400

    Add cmake plugin

---
+++ .config/zsh_custom/plugins/cmake/cmake.plugin.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+alias cmd='cmake -DCMAKE_BUILD_TYPE=Debug ..'
+alias cmdm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make'
+alias cmdmm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make clean && make'
+alias cmr='cmake -DCMAKE_BUILD_TYPE=Debug ..'
+alias cmrm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make'
+alias cmrmm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make clean && make'
