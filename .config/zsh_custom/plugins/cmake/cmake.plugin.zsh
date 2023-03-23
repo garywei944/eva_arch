ed944de43591a8bed93ef3558d945b5f8d9e7e3a
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-03-23 00:55:48 -0400

    cmake and make with -j

--- .config/zsh_custom/plugins/cmake/cmake.plugin.zsh
+++ .config/zsh_custom/plugins/cmake/cmake.plugin.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 alias cmdb='cmake -DCMAKE_BUILD_TYPE=Debug ..'
-alias cmdbm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make'
-alias cmdbmm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make clean && make'
+alias cmdbm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)'
+alias cmdbmm='cmake -DCMAKE_BUILD_TYPE=Debug .. && make clean && make -j$(nproc)'
 alias cmrl='cmake -DCMAKE_BUILD_TYPE=Release ..'
-alias cmrlm='cmake -DCMAKE_BUILD_TYPE=Release .. && make'
-alias cmrlmm='cmake -DCMAKE_BUILD_TYPE=Release .. && make clean && make'
+alias cmrlm='cmake -DCMAKE_BUILD_TYPE=Release .. && make -j$(nproc)'
+alias cmrlmm='cmake -DCMAKE_BUILD_TYPE=Release .. && make clean && make -j$(nproc)'
