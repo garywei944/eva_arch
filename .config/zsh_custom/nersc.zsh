27f4f144c3712c1d4688f432469a98be341cad8c
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-03-22 22:48:57 -0400

    Correctly source module

--- .config/zsh_custom/nersc.zsh
+++ .config/zsh_custom/nersc.zsh
@@ -14,32 +14,5 @@ alias pcpunoden='salloc -q interactive -t 01:00:00 -C cpu -A m4341 -N'
 
 [[ -n ${SLURM_NODELIST+x} ]] && export NODENAME=$SLURM_NODELIST
 
-# module workaround
-# https://github.com/ACAD-UofA/Bioinformatics-Wiki/wiki/Modules#module-is-a-shell-function
-module() {
-  if [ -z "${LMOD_SH_DBG_ON+x}" ]; then
-    case "$-" in
-    *v*x*)
-      __lmod_sh_dbg='vx'
-      ;;
-    *v*)
-      __lmod_sh_dbg='v'
-      ;;
-    *x*)
-      __lmod_sh_dbg='x'
-      ;;
-    esac
-  fi
-  if [ -n "${__lmod_sh_dbg:-}" ]; then
-    set +$__lmod_sh_dbg
-    echo "Shell debugging temporarily silenced: export LMOD_SH_DBG_ON=1 for Lmod's output" 1>&2
-  fi
-  eval "$($LMOD_CMD $LMOD_SHELL_PRGM "$@")" && eval "$(${LMOD_SETTARG_CMD:-:} -s sh)"
-  __lmod_my_status=$?
-  if [ -n "${__lmod_sh_dbg:-}" ]; then
-    echo "Shell debugging restarted" 1>&2
-    set -$__lmod_sh_dbg
-  fi
-  unset __lmod_sh_dbg
-  return $__lmod_my_status
-}
+# Workaround for sourcing module
+. /etc/profile.d/zz-cray-pe.sh
