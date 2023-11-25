6b1e83c90f6a30fb3b33b60d778e70e2751f1c6f
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-11-25 01:55:29 -0500

    Fix cuvpn not working for anyconnect

--- .config/zsh_custom/g2.zsh
+++ .config/zsh_custom/g2.zsh
@@ -4,6 +4,10 @@ alias g2login='ssh gw338@g2-login.coecis.cornell.edu'
 alias gcpulow='srun -p default_partition-interactive --pty -n 4 --mem=8g /bin/zsh -li'
 alias gcpunode='srun -p desa-interactive /bin/zsh -li'
 
-alias cuvpn='/opt/cisco/anyconnect/bin/vpn -s connect cuvpn.cuvpn.cornell.edu'
-alias cudisc='/opt/cisco/anyconnect/bin/vpn -s disconnect'
-alias custate='/opt/cisco/anyconnect/bin/vpn -s state'
+# anyconnect has been failed on arch linux
+# alias cuvpn='/opt/cisco/anyconnect/bin/vpn -s connect cuvpn.cuvpn.cornell.edu'
+# alias cudisc='/opt/cisco/anyconnect/bin/vpn -s disconnect'
+# alias custate='/opt/cisco/anyconnect/bin/vpn -s state'
+
+alias cuvpn="tmux new -s cuvpn 'sudo openconnect cuvpn.cuvpn.cornell.edu -u gw338'"
+alias tcuvpn="tmux a -t cuvpn"
