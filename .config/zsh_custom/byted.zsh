2b601d1165e58775e224cb7e60b96a06e3b07efa
garywei944 <33930674+garywei944@users.noreply.github.com>
2025-02-28 04:50:26 +0000

    Add black-erdos alias

--- .config/zsh_custom/byted.zsh
+++ .config/zsh_custom/byted.zsh
@@ -2,3 +2,9 @@
 
 alias ctlag="code tunnel --name lagrange-tf"
 alias ctorch="code tunnel --name torch"
+
+black-erdos() {
+    for _path in trainer/common/launcher trainer/worker norbert/driver/coordinator; do
+        bash black-format.sh "$_path"
+    done
+}
